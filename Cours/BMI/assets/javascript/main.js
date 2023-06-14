673246bbd7bfa561b3c702e231e7f3c2f8fbd0c4
Cyril Jodar <jodarcyril.pro@gmail.com>
2023-06-14 11:37:40 +0200

    + version final fonctionel et optimisé

--- Cours/BMI/assets/javascript/main.js
+++ Cours/BMI/assets/javascript/main.js
@@ -1,57 +1,3 @@
-// const mainBtns = document.querySelectorAll(".main-btn");
-// const mainContainer = document.querySelector(".main-container");
-// const inputColor = document.querySelectorAll(".input-color");
-// const submitBtn = document.querySelector(".submit-btn");
-// const spanTxt = document.querySelector(".spanTxt");
-// // #222354 #212831 #542243
-
-
-// let nbClick = 1
-
-// let arrayColors = [];
-
-// let objectInputs = {
-//     red : "",
-//     green : "",
-//     blue : ""
-// }
-
-// let advancedColorObject = {
-//     red : "100",
-//     green : "100",
-//     blue : "100"
-// }
-
-// mainBtns.forEach(ele => {
-//     ele.addEventListener("click", onClick);
-// });
-// submitBtn.addEventListener("click", onClickSubmit);
-
-// function onClick(event){
-//     advancedColorObject[event.target.getAttribute("data-color")] = Math.floor(Math.random()*255);
-//     console.log(advancedColorObject);
-//     mainContainer.style.backgroundColor = `rgb(${advancedColorObject["red"]},${advancedColorObject["green"]},${advancedColorObject["blue"]})`; 
-// }
-
-// function onClickSubmit(){
-//     inputColor.forEach(ele => {
-//         objectInputs[ele.getAttribute("data-color")] = ele.value;
-//     })
-//     mainContainer.style.backgroundColor = `rgb(${objectInputs["red"]},${objectInputs["green"]},${objectInputs["blue"]})`;
-//     spanTxt.textContent = `${objectInputs["red"]},${objectInputs["green"]},${objectInputs["blue"]}`
-// }
-
-const inputIMC = document.querySelectorAll(".inputIMC");
-const submitIMC = document.querySelector(".submitIMC");
-const resultIMC = document.querySelector(".resultIMC");
-const enAttenteIMC = document.querySelector(".enAttenteIMC");
-
-let infoPersonne = {
-    taille : 0,
-    poids : 0,
-    imc : 0
-}
-
 const BMIData = [
     { name: "Maigreur", color: "midnightblue", range: [0, 18.5] },
     { name: "Bonne santé", color: "green", range: [18.5, 25] },
@@ -59,39 +5,48 @@ const BMIData = [
     { name: "Obésité modérée", color: "orange", range: [30, 35] },
     { name: "Obésité sévère", color: "crimson", range: [35, 40] },
     { name: "Obésité morbide", color: "purple", range: 40 }
-  ];
+];
 
-submitIMC.addEventListener("click", resultContent)
 
-function getBMI(){
-    inputIMC.forEach(ele =>{
-        infoPersonne[ele.getAttribute("data-personne")] = ele.value;
-    })
+const inputIMC = document.querySelectorAll(".inputIMC");
+const submitIMC = document.querySelector(".submitIMC");
+const resultIMC = document.querySelector(".resultIMC");
+const enAttenteIMC = document.querySelector(".enAttenteIMC");
 
-    if(infoPersonne["poids"] < 40 || infoPersonne["taille"] < 100){
-        handleError();
-    } else{
-        infoPersonne["imc"] = (infoPersonne["poids"] / ((infoPersonne["taille"]/100) **2)).toFixed(2);
+submitIMC.addEventListener("click", getBMI)
+
+
+function getBMI() {
+
+    const height = inputIMC[0];
+    const weight = inputIMC[1];
+    console.log()
+    if (!height.value || !weight.value || height.value <= 0 || weight.value <= 0) {
+        handleError()
+        return
     }
+    const bmi = (weight.value / Math.pow((height.value / 100), 2)).toFixed(1)
+    resultContent(bmi)
 }
 
-function resultContent(){
-    getBMI();
-    resultIMC.textContent = infoPersonne["imc"];
-    for(i=0;i<BMIData.length;i++){
-        if(infoPersonne["imc"] > BMIData[i].range[0] && infoPersonne["imc"] <= BMIData[i].range[1]){
-            resultIMC.style.color = BMIData[i].color;
-            enAttenteIMC.textContent = BMIData[i].name;
-
-        } else if(infoPersonne["imc"] > BMIData[i].range){
-            resultIMC.style.color = BMIData[i].color;
-            enAttenteIMC.textContent = BMIData[i].name;
+function resultContent(value) {
+    let rank;
+    for (let i = 0; i < BMIData.length; i++) {
+        const data = BMIData[i];
+        if (value > data.range[0] && value <= data.range[1]) {
+            rank = data
+            break
+        } else if (data.range >= 40) {
+            rank = data;
         }
     }
+    resultIMC.textContent = value;
+    resultIMC.style.color = rank.color;
+    enAttenteIMC.textContent = rank.name;
 }
 
-function handleError(){
-    resultIMC.textContent = 0;
-    enAttenteIMC.textContent = `Les valeurs entrées ne sont pas valides`;
-    resultIMC.style.color = "red";
+function handleError() {
+    resultIMC.textContent = "Erreur"
+    resultIMC.style.color = "crimson"
+    enAttenteIMC.textContent = "Remplis bien le formulaire bouffon"
 }

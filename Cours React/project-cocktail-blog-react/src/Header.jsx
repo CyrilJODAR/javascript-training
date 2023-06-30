64c5a00664cc79955f579b9f39143beb4d927f52
Cyril Jodar <jodarcyril.pro@gmail.com>
2023-06-30 14:12:35 +0200

    +added nav

--- Cours React/project-cocktail-blog-react/src/Header.jsx
+++ Cours React/project-cocktail-blog-react/src/Header.jsx
@@ -1,15 +1,16 @@
 const Header = ({userFromApi}) =>{
     return(
         <header className="myHeaderTop">
-            <ul>
-                <li><a href="#blank"><img src="/logo192.png" alt="logo" /></a></li>
-                <li><h2>{userFromApi.firstName} {userFromApi.lastName}'s Cocktail Blog</h2></li>
-                <li><a href="#blank">Menu1</a></li>
-                <li><a href="#blank">Menu2</a></li>
-                <li><a href="#blank">Menu3</a></li>
-            </ul>
+            <nav>
+                <ul>
+                    <li><a href="#blank"><img src="/logo192.png" alt="logo" /></a></li>
+                    <li><h2>{userFromApi.firstName} {userFromApi.lastName}'s Cocktail Blog</h2></li>
+                    <li><a href="#blank">Menu1</a></li>
+                    <li><a href="#blank">Menu2</a></li>
+                    <li><a href="#blank">Menu3</a></li>
+                </ul>
+            </nav>
         </header>
     )
 }
-
 export default Header

cb29782e151c110a8997108ee29dc4d479789955
Cyril Jodar <jodarcyril.pro@gmail.com>
2023-07-11 10:26:04 +0200

    swapped welcome to Welcome

--- Cours React/project-coworking-react/src/App.js
+++ Cours React/project-coworking-react/src/App.js
@@ -3,7 +3,7 @@ import AllHighRatedCoworkingItems from './AllHighRatedCoworkingItems';
 import './App.css';
 import Footer from './Footer';
 import Header from './Header';
-import Welcome from './Welcome';
+import Welcome from './WelcomeMessage';
 
 function App() {
 
